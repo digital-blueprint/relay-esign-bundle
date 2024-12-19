005e5b6887059ce7ef3cef57263821062d143a96
Christoph Reiter <reiter.christoph@gmail.com>
2024-12-19 11:17:42 +0100

    config: ensure that profile names are unique
    Across both types, as we want to use the name in the future
    in authz expressions to identify the profile.

--- src/Configuration/BundleConfig.php
+++ src/Configuration/BundleConfig.php
@@ -16,6 +16,36 @@ class BundleConfig
     public function setConfig(array $config): void
     {
         $this->config = $config;
+        $this->ensureUniqueProfileNames();
+    }
+
+    private function ensureUniqueProfileNames(): void
+    {
+        $names = [];
+        foreach ($this->getProfiles() as $profile) {
+            if (in_array($profile->getName(), $names, true)) {
+                throw new \RuntimeException('esign: profile names need to be unique');
+            }
+            $names[] = $profile->getName();
+        }
+    }
+
+    /**
+     * @return Profile[]
+     */
+    public function getProfiles(): array
+    {
+        $profiles = [];
+        $qualifiedConfig = $this->getQualified();
+        if ($qualifiedConfig !== null) {
+            $profiles = $qualifiedConfig->getProfiles();
+        }
+        $advancedConfig = $this->getAdvanced();
+        if ($advancedConfig !== null) {
+            $profiles = array_merge($profiles, $advancedConfig->getProfiles());
+        }
+
+        return $profiles;
     }
 
     public function getQualified(): ?QualifiedConfig
