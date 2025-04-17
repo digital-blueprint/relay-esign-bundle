8e17d8c68febfc62cee571c7e227850f19df120c
Christoph Reiter <reiter.christoph@gmail.com>
2025-04-17 14:39:57 +0200

    ElectronicSignature: port to PHP attributes

--- src/Entity/ElectronicSignature.php
+++ src/Entity/ElectronicSignature.php
@@ -4,37 +4,80 @@ declare(strict_types=1);
 
 namespace Dbp\Relay\EsignBundle\Entity;
 
+use ApiPlatform\Metadata\ApiProperty;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\OpenApi\Model\Operation;
+use Dbp\Relay\EsignBundle\State\DummyVerifyProvider;
 use Symfony\Component\Serializer\Annotation\Groups;
 
+#[ApiResource(
+    shortName: 'EsignElectronicSignature',
+    description: 'An electronic signature of a signed document',
+    types: ['http://schema.tugraz.at/ElectronicSignature'],
+    operations: [
+        new GetCollection(
+            uriTemplate: '/electronic-signatures',
+            openapi: new Operation(
+                tags: ['Electronic Signatures']
+            ),
+            provider: DummyVerifyProvider::class
+        ),
+        new Get(
+            uriTemplate: '/electronic-signatures/{identifier}',
+            openapi: new Operation(
+                tags: ['Electronic Signatures']
+            ),
+            provider: DummyVerifyProvider::class
+        ),
+    ],
+    formats: [
+        0 => 'jsonld',
+        // for backwards compat we also support json here
+        'json' => ['application/json'],
+    ],
+    routePrefix: '/esign',
+    normalizationContext: [
+        'groups' => ['EsignElectronicSignature:output'],
+    ]
+)]
 class ElectronicSignature
 {
+    #[ApiProperty(identifier: true)]
     #[Groups(['EsignElectronicSignature:output'])]
     private $identifier;
 
+    #[ApiProperty(iris: ['http://schema.org/givenName'])]
     #[Groups(['EsignElectronicSignature:output'])]
     private $givenName;
 
     /**
      * @var string
      */
+    #[ApiProperty(iris: ['https://schema.org/familyName'])]
     #[Groups(['EsignElectronicSignature:output'])]
     private $familyName;
 
     /**
      * @var string
      */
+    #[ApiProperty(iris: ['https://schema.org/serialNumber'])]
     #[Groups(['EsignElectronicSignature:output'])]
     private $serialNumber;
 
     /**
      * @var string
      */
+    #[ApiProperty(iris: ['https://schema.org/Text'])]
     #[Groups(['EsignElectronicSignature:output'])]
     private $signedBy;
 
+    #[ApiProperty(iris: ['https://schema.org/nationality'])]
     #[Groups(['EsignElectronicSignature:output'])]
     private $nationality;
 
+    #[ApiProperty(iris: ['https://schema.org/Text'])]
     #[Groups(['EsignElectronicSignature:output'])]
     private $valueMessage;
 
