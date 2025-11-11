3328cf65b8728f2b9d55a80a335f009decfe3321
Christoph Reiter <reiter.christoph@gmail.com>
2025-11-11 15:31:01 +0100

    tests: port away from UserAuthTrait
    it's deprecated

--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -5,17 +5,10 @@ declare(strict_types=1);
 namespace Dbp\Relay\EsignBundle\Tests;
 
 use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
-use Dbp\Relay\CoreBundle\TestUtils\UserAuthTrait;
+use Dbp\Relay\CoreBundle\TestUtils\TestClient;
 
 class ApiTest extends ApiTestCase
 {
-    use UserAuthTrait;
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-    }
-
     public function testNotAuth()
     {
         $endpoints = [
@@ -33,24 +26,21 @@ class ApiTest extends ApiTestCase
             ['GET', '/esign/qualifiedly-signed-documents/123', 403],
         ];
 
+        $client = $this->createClient();
+        $client->disableReboot();
+
         foreach ($endpoints as $ep) {
             [$method, $path, $status] = $ep;
-            $client = $this->withUser('foobar', [], '42');
-            $client->disableReboot();
-
-            $headers = [
-                'Authorization' => 'Bearer 42',
-            ];
+            $testClient = new TestClient($client);
+            $testClient->setUpUser('foobar', token: '42');
             if ($method === 'POST') {
                 $headers['Content-Type'] = 'multipart/form-data';
             }
-            $response = $client->request($method, $path, ['headers' => $headers]);
-
+            $response = $testClient->request($method, $path, token: '42');
             $this->assertEquals($status, $response->getStatusCode(), $path);
 
             // Without any token
-            unset($headers['Authorization']);
-            $response = $client->request($method, $path, ['headers' => $headers]);
+            $response = $testClient->request($method, $path, token: null);
             $this->assertContains($response->getStatusCode(), [401, 404, 403], $path);
         }
     }
