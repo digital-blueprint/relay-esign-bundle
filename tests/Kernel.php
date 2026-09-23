1978567f0cd57a120257112e008a079e65cb5644
Christoph Reiter <reiter.christoph@gmail.com>
2026-09-23 12:00:59 +0200

    Port to CoreTestKernelTrait

--- tests/Kernel.php
+++ tests/Kernel.php
@@ -4,47 +4,16 @@ declare(strict_types=1);
 
 namespace Dbp\Relay\EsignBundle\Tests;
 
-use ApiPlatform\Symfony\Bundle\ApiPlatformBundle;
-use Dbp\Relay\CoreBundle\DbpRelayCoreBundle;
+use Dbp\Relay\CoreBundle\TestUtils\CoreTestKernelTrait;
 use Dbp\Relay\EsignBundle\DbpRelayEsignBundle;
-use Nelmio\CorsBundle\NelmioCorsBundle;
-use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
-use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
-use Symfony\Bundle\MonologBundle\MonologBundle;
-use Symfony\Bundle\SecurityBundle\SecurityBundle;
-use Symfony\Bundle\TwigBundle\TwigBundle;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
-use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 class Kernel extends BaseKernel
 {
-    use MicroKernelTrait;
+    use CoreTestKernelTrait;
 
-    public function registerBundles(): iterable
+    protected function registerAdditionalBundles(): iterable
     {
-        yield new FrameworkBundle();
-        yield new SecurityBundle();
-        yield new TwigBundle();
-        yield new NelmioCorsBundle();
-        yield new MonologBundle();
-        yield new ApiPlatformBundle();
         yield new DbpRelayEsignBundle();
-        yield new DbpRelayCoreBundle();
-    }
-
-    protected function configureRoutes(RoutingConfigurator $routes)
-    {
-        $routes->import('@DbpRelayCoreBundle/Resources/config/routing.yaml');
-    }
-
-    protected function configureContainer(ContainerConfigurator $container)
-    {
-        $container->import('@DbpRelayCoreBundle/Resources/config/services_test.yaml');
-        $container->extension('framework', [
-            'test' => true,
-            'secret' => 'something',
-            'annotations' => false,
-        ]);
     }
 }
